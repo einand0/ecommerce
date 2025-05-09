34f5b49cdf20562ef8b5ba363fd92a57bd23141a
Fernando Lopes <87499075+einand0@users.noreply.github.com>
2025-05-08 23:23:32 -0300

    feat: User and Role entities created

---
+++ src/main/java/com/einando/ecommerce/model/Role.java
@@ -0,0 +1,22 @@
+package com.einando.ecommerce.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_roles")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long role_id;
+    private String name;
+}
---
+++ src/main/java/com/einando/ecommerce/model/User.java
@@ -0,0 +1,25 @@
+package com.einando.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID user_id;
+    private String name;
+    private String email;
+    private String password;
+}
